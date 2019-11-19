f2f74409969d3726d82c105c2b4a797b6644c42f
LaTahsin <48186962+LaTahsin@users.noreply.github.com>
2019-11-20 01:17:02 +0600

    Update visualization_utils.py

--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -205,7 +205,7 @@ def draw_bounding_box_on_image(image,
   draw.line([(left, top), (left, bottom), (right, bottom),
              (right, top), (left, top)], width=thickness, fill=color)
   try:
-    font = ImageFont.truetype('arial.ttf', 24)
+    font = ImageFont.truetype('arial.ttf', 30)
   except IOError:
     font = ImageFont.load_default()
 
